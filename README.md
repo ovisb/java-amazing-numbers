5ab7d2da65f525f4c9dae0494106d53e4a901fe7
Ovidiu <ovidiu.sbangu@gmail.com>
2023-11-13 18:17:44 +0200

    completed stage 3

--- README.md
+++ README.md
@@ -15,12 +15,18 @@ Program which will provide some useful information about a natural number.
 To add...
 
 ## Changelog
-07.11.2023
+10.11.2023
 - Ask user for number
 - Check if it's natural, if not raise error
-- Calculate whether number is odd or even and print message
-- Check also if number is a Buzz number
-- Print explanations regarding number being Buzz
+- Calculate whether number is `odd` or `even` and print message
+- Check also if number is a `Buzz` number
+- Print explanations regarding number being `Buzz`
+
+13.11.2023
+- Add a new method which checks if number is a `Duck`
+- A `Duck` number is any number which contains a `0` (zero)
+- Added new fields to store state of each natural number, whether it's odd/even/buzz/duck
+- Instead of all the print statements, there is one method which prints all the necessary info regarding the number object.
 
 ## Project status
 
--- src/numbers/Number.java
+++ src/numbers/Number.java
@@ -5,15 +5,57 @@ import java.util.Scanner;
 
 class Number {
 
+    private boolean isOdd;
+    private boolean isEven;
+    private boolean isBuzz;
+    private boolean isDuck;
+
+    private int number;
+
+    Number() {
+        this.isOdd = false;
+        this.isEven = false;
+        this.isBuzz = false;
+        this.isDuck = false;
+    }
+
     public void start() {
-        int number;
         try {
-            number = getInt("Enter a natural number: ");
+            this.number = getInt("Enter a natural number: ");
         } catch (Exception e) {
             return;
         }
         checkOddEven(number);
         checkIfBuzz(number);
+        checkDuck(number);
+        System.out.println(this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Properties of %d
+                        even: %b
+                        odd: %b
+                        buzz: %b
+                        duck: %b
+                """,
+                this.number,
+                this.isEven,
+                this.isOdd,
+                this.isBuzz,
+                this.isDuck
+                );
+    }
+
+    private void checkDuck(int number) {
+        while (number > 0) {
+            if (number % 10 == 0) {
+                this.isDuck = true;
+                return;
+            }
+            number /= 10;
+        }
     }
 
     private void validateInt(int number) throws Exception {
@@ -39,28 +81,33 @@ class Number {
 
     private void checkOddEven(int number) {
         if (number % 2 == 0) {
-            System.out.println("The number is Even.");
+//            System.out.println("The number is Even.");
+            isEven = true;
         } else {
-            System.out.println("The number is Odd.");
+//            System.out.println("The number is Odd.");
+            isOdd = true;
         }
     }
     private void checkIfBuzz(int number) {
         String message;
         if (number % 7 == 0 && number % 10 == 7) {
-            System.out.println("It is a Buzz number.");
-            message = String.format("%d number is divisible by 7 and ends with 7.", number);
+//            System.out.println("It is a Buzz number.");
+//            message = String.format("%d number is divisible by 7 and ends with 7.", number);
+            isBuzz = true;
         }
         else if (number % 7 == 0) {
-            System.out.println("It is a Buzz number.");
-            message = String.format("%d is divisible by 7.", number);
+//            System.out.println("It is a Buzz number.");
+//            message = String.format("%d is divisible by 7.", number);
+            isBuzz = true;
         } else if (number % 10 == 7) {
-            System.out.println("It is a Buzz number.");
-            message = String.format("%d ends with 7.", number);
+//            System.out.println("It is a Buzz number.");
+//            message = String.format("%d ends with 7.", number);
+            isBuzz = true;
         } else {
-            System.out.println("It is not a Buzz number.");
-            message = String.format("%d is neither divisible by 7 nor does it end with 7", number);
+//            System.out.println("It is not a Buzz number.");
+//            message = String.format("%d is neither divisible by 7 nor does it end with 7", number);
         }
-        System.out.println("Explanation:");
-        System.out.println(message);
+//        System.out.println("Explanation:");
+//        System.out.println(message);
     }
 }
